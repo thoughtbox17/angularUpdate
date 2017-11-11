5245210289170384b3d2bc0f666d8c1a5d5e999c
ConanKeaveney <conankeaveney96@gmail.com>
2017-11-11 17:20:11 +0000

    angularfire

--- builds/angularregistration/js/controllers/uploadfile.js
+++ builds/angularregistration/js/controllers/uploadfile.js
@@ -1,6 +1,35 @@
 //var myApp = angular.module('myApp', ['firebase', 'angular.filter']);
 
-myApp.controller("UploadFileController",['$scope','$firebaseArray',
+// define our app and dependencies (remember to include firebase!)
+angular
+.module("myApp", [
+  "firebase"
+])
+.controller("UploadFileController", UploadFileController);
+
+// inject $firebaseStorage into our controller
+function UploadFileController($firebaseStorage) {
+  // create a Storage reference for the $firebaseStorage binding
+  var storageRef = firebase.storage().ref("userProfiles/files");
+  var storage = $firebaseStorage(storageRef);
+  //var file = // get a file from the template (see Retrieving files from template section below)
+  var uploadTask = storage.$put(file);
+  // of upload via a RAW, base64, or base64url string
+  var stringUploadTask = storage.$putString('5b6p5Y+344GX44G+44GX44Gf77yB44GK44KB44Gn44Go44GG77yB', 'base64');
+}
+UploadFileController.$inject = ["$firebaseStorage"];
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+
+/*myApp.controller("UploadFileController",['$scope','$firebaseArray',
 function($scope, $firebaseArray) {
   
   var ref = firebase.storage().ref();
@@ -62,4 +91,4 @@ function($scope, $firebaseArray) {
     }
   }
 
-}]);
+}]);*/
